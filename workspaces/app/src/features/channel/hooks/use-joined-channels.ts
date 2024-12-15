2b1096ec6baf88c644aef51f03f4fcb544c2b948
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-16 01:01:25 +0900

    refactor(channel): simplify by empty schema (#11)

--- workspaces/app/src/features/channel/hooks/use-joined-channels.ts
+++ workspaces/app/src/features/channel/hooks/use-joined-channels.ts
@@ -1,16 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRepositories } from "@/hooks/repository";
-import { create } from "@bufbuild/protobuf";
-import { GetAllChannelsRequestSchema } from "@meline/schema/schema/request/channel_request_pb.js";
 
 export const useJoinedChannels = () => {
   const { channelRepository } = useRepositories();
 
   return useQuery({
     queryKey: channelRepository.getJoinedChannels$$key(),
-    queryFn: () =>
-      channelRepository.getJoinedChannels(
-        create(GetAllChannelsRequestSchema, {}),
-      ),
+    queryFn: () => channelRepository.getJoinedChannels(),
   });
 };
